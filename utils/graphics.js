57dbe1e8eec95ac45413fa50ed77577c65b85f2d
Shashvath Srivatsa <shashvathsrivatsa@Shashvaths-MacBook-Pro.local>
2026-01-31 00:55:12 -0500

    changed to woff2

--- utils/graphics.js
+++ utils/graphics.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const fontResource = loadFontResource('SF-Pro-Display-Medium.otf');
+const fontResource = loadFontResource('SF-Pro-Display-Medium.woff2');
 
 
 
